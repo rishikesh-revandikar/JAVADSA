6052df2bcada30422f90b3fe17e5bfaf79b425b4
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-17 20:15:36 +0530

    New code

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import Recursion.ReverseArray;
 import Recursion.SumOfN;
+import Sorting.SortingMain;
 
 public class Main {
     public static void main(String[] args) {
-        ReverseArray r = new ReverseArray();
-
-        r.call();
+        SortingMain s = new SortingMain();
+        s.callSortingFunction(1);
     }
 }
