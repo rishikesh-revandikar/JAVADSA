c296fb7744f1221930210868f8458df2b285b218
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-28 23:47:31 +0530

    Linked List basics

---
+++ LinkedList/LLmain.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class LLmain {
+
+    public LLmain(){
+        int []arr = {1,2,3,4};
+
+        Node n = new Node(arr[3]);
+        System.out.println(n.next);
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import BinarySearch.BSmain;
+import LinkedList.LLmain;
 import Recursion.ReverseArray;
 import Recursion.SumOfN;
 import Sorting.SortingMain;
 
 public class Main {
     public static void main(String[] args) {
-        BSmain b = new BSmain();
-        b.callBSFunctions();
+        LLmain l = new LLmain();
+
     }
 }
