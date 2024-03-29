1c934633e13396fe0186796638239a1da3eb3d81
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-29 10:57:43 +0530

    Linked List Deletion

--- LinkedList/LLBasics.java
+++ LinkedList/LLBasics.java
@@ -24,5 +24,69 @@ public class LLBasics {
         System.out.println();
     }
 
+    //Deletion in Linked List
+    public static Node removeHead(Node head){
+        if(head==null) return head;
+        head = head.next;
+        return head;
+    }
+
+    public static Node removeTail(Node head){
+        if(head==null || head.next==null) return null;
+        Node temp = head;
+        while (temp.next.next!=null){
+            temp=temp.next;
+        }
+        temp.next=null;
+        return head;
+    }
+
+    public static Node removeKthElement(Node head,int k){
+        if(head==null) return head;
+        if(k==1){
+            head = head.next;
+            return head;
+        }
+        int counter = 0;
+        Node temp = head;
+        Node prev = null;
+
+        while (temp != null){
+            counter++;
+            if(k == counter){
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static Node removeByValue(Node head, int val){
+        if(head==null) return head;
+        if(head.data == val){
+            head = head.next;
+            return head;
+        }
+
+        Node temp = head;
+        Node prev = null;
+
+        while (temp != null){
+
+            if(temp.data == val){
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+
+
+
 
 }
--- LinkedList/LLmain.java
+++ LinkedList/LLmain.java
@@ -6,6 +6,12 @@ public class LLmain {
     public void callLLFunctions(){
         Node head = LLBasics.convertArrayToLL(arr);
         LLBasics.printLL(head);
+//        Node newHead= LLBasics.removeHead(head);
+//        Node newNode = LLBasics.removeTail(head);
+//        Node newHead = LLBasics.removeKthElement(head,4);
+        Node newHead = LLBasics.removeByValue(head,3);
+        LLBasics.printLL(newHead);
+
     }
 
 }
