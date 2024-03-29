216c0f2a363e61f6e68abcf498178ea6d02306bd
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-29 11:33:27 +0530

    Linked List Insertion

--- LinkedList/LLBasics.java
+++ LinkedList/LLBasics.java
@@ -85,8 +85,85 @@ public class LLBasics {
         return head;
     }
 
+    // Insertion in Linked List
+    public static Node insertAtHead(Node head,int val){
+        return new Node(val,head);
+    }
+
+    public static Node insertAtTail(Node head, int val){
+        if(head == null) return new Node(val);
+
+        Node temp = head;
+
+        while (temp.next != null){
+            temp = temp.next;
+        }
+
+        temp.next = new Node(val);
+        return head;
+    }
+
+    public static Node insertAtKthPosition(Node head, int val, int k){
+        if(head == null){
+            if(k==1){
+                return new Node(val);
+            }
+            return null;
+        }
+
+        if(k==1){
+            return new Node(val,head);
+        }
+
+        int counter = 0;
+        Node temp = head;
+
+        while (temp != null){
+            counter++;
+
+            if(counter == k - 1){
+                Node newNode = new Node(val);
+                newNode.next = temp.next;
+                temp.next = newNode;
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        return head;
+    }
+
+    public static Node insertBeforeValue(Node head, int val, int target){
+        boolean flag = false;
+        if(head == null){
+            return null;
+        }
+
+        if(head.data == target){
+            flag = true;
+            return new Node(val,head);
+        }
 
 
+        Node temp = head;
 
+        while (temp.next != null){
+
+            if(temp.next.data == target){
+                Node newNode = new Node(val);
+                newNode.next = temp.next;
+                temp.next = newNode;
+                flag = true;
+                break;
+            }
+
+            temp = temp.next;
+        }
+        if(!flag){
+            System.out.println("Target element not found");
+        }
+        return head;
+    }
 
 }
--- LinkedList/LLmain.java
+++ LinkedList/LLmain.java
@@ -5,12 +5,18 @@ public class LLmain {
 
     public void callLLFunctions(){
         Node head = LLBasics.convertArrayToLL(arr);
+//        Node head = null;
         LLBasics.printLL(head);
 //        Node newHead= LLBasics.removeHead(head);
 //        Node newNode = LLBasics.removeTail(head);
 //        Node newHead = LLBasics.removeKthElement(head,4);
-        Node newHead = LLBasics.removeByValue(head,3);
+//        Node newHead = LLBasics.removeByValue(head,3);
+//        Node newHead = LLBasics.insertAtHead(head,0);
+//        Node newHead = LLBasics.insertAtTail(head,9);
+//        Node newHead = LLBasics.insertAtKthPosition(head, 9, 5);
+        Node newHead = LLBasics.insertBeforeValue(head,9, 6);
         LLBasics.printLL(newHead);
+
 
     }
 
